04702eb77b4ed47f13e28151d3f57bc66c3060f1
HaiderAliMuazzam <haiderali34436@gmail.com>
2026-07-24 16:57:51 +0500

    Add Reverb broadcasting config and Echo setup

---
+++ routes/channels.php
@@ -0,0 +1,11 @@
+<?php
+use Illuminate\Support\Facades\Broadcast;
+use Illuminate\Support\Facades\Gate;
+
+Broadcast::channel('App.Models.User.{id}', function ($user, $id) {
+    return (int) $user->id === (int) $id;
+});
+
+Broadcast::channel('admin.students', function ($user) {
+    return Gate::forUser($user)->allows('manage-students');
+});
